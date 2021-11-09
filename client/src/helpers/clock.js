31628c01ef1339f86358e73d0a4c15b9f7797e1f
Mogador <jcaillaud17170@gmail.com>
2021-11-09 21:29:49 +0100

    Include minutes displaying

--- client/src/helpers/clock.js
+++ client/src/helpers/clock.js
@@ -1,24 +1,31 @@
 export default class Clock {
     constructor(scene, x, y, isRunning = true) {
-        this.text = scene.add.text(x, y, "").setFontSize(15).setFontFamily('Trebuchet MS').setColor('#ffffff');
+        this.text = scene.add.text(x, y, "").setFontSize(15).setFontFamily('Arial').setColor('#ffffff');
         this.isRunning = isRunning;
+        this.minutes = 0;
+
+        const self = this;
 
         this.timer = scene.time.addEvent({
+            delay: 60000,
             loop: true,
-            paused: !isRunning
+            paused: !isRunning,
+            callback: () => {
+                self.minutes++;
+            }
         });
     }
 
     update() {
         if(this.isRunning) {
-            this.text.setText(Math.floor(this.timer.getElapsed())/1000);
+            this.text.setText(this.minutes + ' min ' + Math.floor(this.timer.getElapsed())/1000);
         }
     }
 
     stop() {
         this.timer.paused = true;
         this.isRunning = false;
-        this.text.setText(Math.floor(this.timer.getElapsed())/1000);
+        this.text.setText(this.minutes + ' min ' + Math.floor(this.timer.getElapsed())/1000);
     }
 
 }
