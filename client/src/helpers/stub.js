f22d684b7151e8e942f69a18fbe45500ce3b7e82
Julien <jcaillaud17170@gmail.com>
2021-11-18 15:55:22 +0100

    Stub - Move cards

--- client/src/helpers/stub.js
+++ client/src/helpers/stub.js
@@ -13,7 +13,8 @@ export default class Stub extends Stack {
         super(x, y, callbackLastCard);
 
         this.nb_cards = nb_cards;
-        this.prev_cards = [];
+
+        this.deltaX = 20;
     }
 
     addCards(cards) {
@@ -25,10 +26,28 @@ export default class Stub extends Stack {
 
         let i = 0;
         for(const card of cards) {
-            card.sprite.setX(this.x + 20 * i);
+            card.sprite.setX(this.x + this.deltaX * i);
             i++;
         }
 
-        this.setupLastCard();
+        this.setupLastCard(false);
+
+        this.last_card_x = this.last_card.sprite.x;
+    }
+
+    setupLastCard(move_cards = true) {
+        super.setupLastCard();
+
+        if(move_cards) {
+            if(this.cards.length > this.nb_cards - 1) {
+                if(this.last_card.sprite.x < this.last_card_x) {
+                    let i = 0;
+                    for(const card of this.cards.slice(-this.nb_cards)) {
+                        card.sprite.setX(this.x + this.deltaX * i);
+                        i++;
+                    }
+                }
+            }
+        }
     }
 }
