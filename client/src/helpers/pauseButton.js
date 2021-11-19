60ac04ebf4a58784aa1e730ca302ed04c3628ca0
Julien <jcaillaud17170@gmail.com>
2021-11-19 13:06:36 +0100

    PauseButton - Send all scene as param to pause Scene

--- client/src/helpers/pauseButton.js
+++ client/src/helpers/pauseButton.js
@@ -3,7 +3,7 @@ export default class PauseButton {
         const button = scene.add.bitmapText(scene.cameras.main.width - 20, 20, 'arial', "Pause", 15).setOrigin(1, 0);
 
         button.setInteractive({ useHandCursor: true }).on('pointerdown', function () {
-            scene.scene.launch('Pause', { scene : scene.scene.key });
+            scene.scene.launch('Pause', { scene : scene.scene });
             scene.scene.pause();
         });
     }
