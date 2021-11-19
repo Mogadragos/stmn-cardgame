94a2e3b463f6d12ea34a1e10939fb976f6535539
Julien <jcaillaud17170@gmail.com>
2021-11-19 13:18:35 +0100

    Keyboard - Pause and resume on ESC

--- client/src/scenes/menu.js
+++ client/src/scenes/menu.js
@@ -13,11 +13,13 @@ export default class Menu extends Phaser.Scene {
         this.input.setDefaultCursor('default');
 
         const self = this;
-  
-        this.add.bitmapText(this.cameras.main.centerX, this.cameras.main.centerY - 60, 'arial', "Reprendre", 30).setOrigin(0.5).setInteractive({ useHandCursor: true }).once('pointerdown', function () {
+
+        const resume = () => {
             data.scene.resume();
             self.scene.stop();
-        });
+        }
+  
+        this.add.bitmapText(this.cameras.main.centerX, this.cameras.main.centerY - 60, 'arial', "Reprendre", 30).setOrigin(0.5).setInteractive({ useHandCursor: true }).once('pointerdown', resume);
 
         this.add.bitmapText(this.cameras.main.centerX, this.cameras.main.centerY, 'arial', "Recommencer", 30).setOrigin(0.5).setInteractive({ useHandCursor: true }).once('pointerdown', function () {
             data.scene.restart();
@@ -29,6 +31,8 @@ export default class Menu extends Phaser.Scene {
             self.scene.wake('ChooseMode');
             self.scene.stop();
         });
+
+        this.input.keyboard.on('keydown-ESC', resume);
     }
     
     update() {
